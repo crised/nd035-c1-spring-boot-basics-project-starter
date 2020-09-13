d7060dd99c5d26111b24b7aeaec8e10fb1f1988f
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-09-13 18:09:14 -0300

    Uploading file and filename correctly

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -2,12 +2,10 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import com.udacity.jwdnd.course1.cloudstorage.model.FileForm;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,11 +20,10 @@ public class FileController {
         try {
             InputStream fis = fileUpload.getInputStream();
             byte[] file = fis.readAllBytes();
-
+            System.out.println(fileUpload.getOriginalFilename());
         } catch (IOException e) {
             System.out.println("Couldn't upload file");
         }
-//        System.out.println("Upload file!: " + fileForm.getFileName());
         System.out.println("Upload file!: " + fileUpload.getSize());
         return "home";
     }
