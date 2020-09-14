0c7bf36c6c163987ae39e870ee51d3b46ffda1a9
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-09-14 08:56:23 -0300

    On the way to implement user support

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,6 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.service.FileService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -14,6 +16,11 @@ public interface UserMapper {
     List<User> getAllUsers();
 
     @Select("SELECT * FROM USERS WHERE username=#{username}")
-    User getUserWithUsername(String username);
+    User getUser(String username);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) " +
+            "VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
 
 }
